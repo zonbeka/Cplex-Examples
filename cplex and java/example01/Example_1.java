993e478257bcb6536d37b5df41617738df49f7a2
Hernan Caceres <zonbeka@gmail.com>
2014-07-05 14:48:14 -0400

    update with video

--- cplex and java/example01/Example_1.java
+++ cplex and java/example01/Example_1.java
@@ -28,24 +28,24 @@ public class Example_1 {
 			constraints.add(cplex.addGe(cplex.sum(cplex.prod(12, x),cplex.prod(6, y)), 36));
 			constraints.add(cplex.addGe(cplex.sum(cplex.prod(10, x),cplex.prod(30, y)), 90));
 			IloLinearNumExpr num_expr = cplex.linearNumExpr();
-			num_expr.addTerm(2,x);
+			num_expr.addTerm(2, x);
 			num_expr.addTerm(-1,y);
 			constraints.add(cplex.addEq(num_expr, 0));
 			num_expr = cplex.linearNumExpr();
 			num_expr.addTerm(1,y);
 			num_expr.addTerm(-1,x);
-			constraints.add(cplex.addLe(num_expr, 8));
-
+			constraints.add(cplex.addLe(num_expr,8));
+			
 			cplex.setParam(IloCplex.Param.Simplex.Display, 0);
 			
 			// solve
 			if (cplex.solve()) {
-				System.out.println("obj          = "+cplex.getObjValue());
-				System.out.println("x            = "+cplex.getValue(x));
-				System.out.println("y            = "+cplex.getValue(y));
-				for (int i=0; i<constraints.size();i++) {
-					System.out.println("dual cons  "+(i+1)+" = "+cplex.getDual(constraints.get(i)));
-					System.out.println("slack cons "+(i+1)+" = "+cplex.getSlack(constraints.get(i)));
+				System.out.println("obj = "+cplex.getObjValue());
+				System.out.println("x   = "+cplex.getValue(x));
+				System.out.println("y   = "+cplex.getValue(y));
+				for (int i=0;i<constraints.size();i++) {
+					System.out.println("dual   constraint "+(i+1)+" = "+cplex.getDual(constraints.get(i)));
+					System.out.println("salack constraint "+(i+1)+" = "+cplex.getSlack(constraints.get(i)));
 				}
 			}
 			else {
@@ -58,5 +58,4 @@ public class Example_1 {
 			exc.printStackTrace();
 		}
 	}
-
 }
